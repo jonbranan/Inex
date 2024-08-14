267616f0e47ff7c87d1d25cbb9fbde106b344eed
Jonathan Branan <jonbranan@gmail.com>
2024-08-14 15:05:27 -0500

    Updated daysTopull value

--- inexSqlquery.py
+++ inexSqlquery.py
@@ -7,5 +7,5 @@ class sqlQuerymodel:
         FROM tbl_Transactions t
              Full JOIN tbl_ProtocolCommands p ON(t.TransactionID=p.TransactionID)
              Full join tbl_Authentications a ON(t.TransactionID=a.TransactionID)
-        WHERE p.Time_stamp>@stopTime AND p.Command IS NOT NULL""".replace("30", daysTopull)
+        WHERE p.Time_stamp>@stopTime AND p.Command IS NOT NULL""".replace("30", str(daysTopull))
         return configQuery if overRideflag else q
