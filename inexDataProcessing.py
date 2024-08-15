c56c9ac208242bc8d1b70ef05a9e65ab0e5153fb
Jonathan Branan <jonbranan@gmail.com>
2024-08-15 14:58:53 -0500

    Fixed auth bug

--- inexDataProcessing.py
+++ inexDataProcessing.py
@@ -4,58 +4,69 @@ def processData(data, template, **kwargs):
     additional data to insert into the template."""
     processedData = []
     transactionLoginid = []
-
+    
     for row in data:
         # print(f'Row: {row}')
-        if identifyUtype(row.get('Command')) == "other":
+        # must set variables for the different templates and do logic based on that. Do not call identifyUtype many times
+        identifyUtypecommand = identifyUtype(row.get('Command'))
+
+        if identifyUtypecommand == "other":
             continue
-        
+
         if row.get('Command') == None:
             continue
+
+        userType = identifyUserType(row.get('user_type'))
         try:
-            processedData.append(template(identifyUtype(row.get('Command')),\
-                prd_ext_tenant_id='',\
+            processedData.append(template(identifyUtypecommand,\
+                prd_ext_tenant_name=kwargs.get('prd_ext_tenant_name'),\
+                user_uid=row.get('TransactionID'),\
+                status_detail=row.get('Description'),\
+                prd_ext_tenant_id=kwargs.get('prd_ext_tenant_id'),\
                 status_code=row.get('ResultID'),\
+                file_created_time=row.get('Time_stamp'),\
                 file_size=row.get('FileSize'),\
+                file_uid=row.get('ProtocolCommandID'),\
                 file_path=row.get('PhysicalFolderName'),\
-                file_virtual_path=row.get('VirtualFolderName'),\
                 file_name=row.get('FileName'),\
                 guid=row.get('TransactionGUID'),\
-                ref_id=row.get('ProtocolCommandID'),\
-                prd_instance_id=kwargs.get('prd_instance_id'),\
-                product_guid=kwargs.get('product_guid'),\
                 product_name=kwargs.get('product_name'),\
-                product_version=kwargs.get('product_version'),\
                 node_name=row.get('NodeName'),\
+                session_uid=row.get('TransactionID'),\
                 src_endpoint_type=row.get('Protocol'),\
                 src_endpoint_port=row.get('RemotePort'),\
                 src_endpoint_ip=row.get('RemoteIP'),\
                 dst_endpoint_port=row.get('LocalPort'),\
                 dst_endpoint_ip=row.get('LocalIP'),\
                 dst_endpoint_type=row.get('Protocol'),\
-                session_uid=row.get('TransactionID'),\
-                bytes_out=row.get('BytesTransferred'),\
-                duration=row.get('TransferTime'),\
+                user_session_uid=row.get('TransactionID'),\
+                bytes=row.get('BytesTransferred'),\
                 time=row.get('Time_stamp'),\
-                user_type=identifyUserType(row.get('user_type')),\
-                user_domain=row.get('SiteName'),\
+                duration=row.get('TransferTime'),\
+                user_type=userType,\
                 user_name=row.get('Actor'),\
                 user_home_directory=row.get('VirtualFolderName'),\
-                description=row.get('Description'),\
-                utype=identifyUtype(row.get('Command'))))
+                utype=identifyUtypecommand))
         except UnboundLocalError:
             print(f'Problem row GUID:{row.get("TransactionGUID")} ::: TransactionObject:{row.get("TransactionObject")} Command: {row.get("Command")}')
+            continue
 
+        identifyUtypetransactionObject = identifyUtype(row.get('TransactionObject'))
+
+        if identifyUtypetransactionObject == "other":
             continue
 
         if row.get('TransactionGUID') not in transactionLoginid:
             try:
-                processedData.append(template(identifyUtype(row.get('TransactionObject')),\
+                processedData.append(template(identifyUtypetransactionObject,\
+                    prd_ext_tenant_id=kwargs.get('prd_ext_tenant_id'),\
+                    prd_ext_tenant_name=kwargs.get('prd_ext_tenant_name'),\
+                    status_detail=row.get('Description'),\
                     guid=row.get('TransactionGUID'),\
+                    status_code=row.get('ResultID'),\
+                    node_name=row.get('NodeName'),\
                     prd_instance_id=kwargs.get('prd_instance_id'),\
-                    product_guid=kwargs.get('product_guid'),\
                     product_name=kwargs.get('product_name'),\
-                    product_version=kwargs.get('product_version'),\
                     src_endpoint_type=row.get('Protocol'),\
                     src_endpoint_port=row.get('RemotePort'),\
                     src_endpoint_ip=row.get('RemoteIP'),\
@@ -63,14 +74,14 @@ def processData(data, template, **kwargs):
                     dst_endpoint_ip=row.get('LocalIP'),\
                     dst_endpoint_type=row.get('Protocol'),\
                     session_uid=row.get('TransactionID'),\
-                    bytes_out=row.get('BytesTransferred'),\
                     transfer_time=row.get('TransferTime'),\
                     time=row.get('Time_stamp'),\
-                    user_type=identifyUserType(row.get('user_type')),\
-                    user_domain=row.get('SiteName'),\
+                    user_session_uid=row.get('TransactionID'),\
+                    user_uid=row.get('TransactionID'),\
+                    user_type=userType,\
                     user_name=row.get('Actor'),\
-                    user_home_directory=row.get('VirtualFolderName'),\
-                    utype=identifyUtype(row.get('TransactionObject'))\
+                    user_home_directory=row.get('PhysicalFolderName'),\
+                    utype=identifyUtypetransactionObject\
                     ))
                 transactionLoginid.append(row.get('TransactionGUID'))
             except UnboundLocalError:
@@ -88,6 +99,7 @@ def identifyUserType(obj):
             return "User"
     else:
         return None
+
 def identifyUtype(obj):
     """Process Type of transaction based on string that passed in.
     Return transaction type."""
@@ -98,11 +110,11 @@ def identifyUtype(obj):
 
     if obj in user_logged_on:
         return "user_logged_on"
-    if obj in file_deleted:
+    elif obj in file_deleted:
         return "file_deleted"
-    if obj in file_uploaded:
+    elif obj in file_uploaded:
         return "file_uploaded"
-    if obj in file_downloaded:
+    elif obj in file_downloaded:
         return "file_downloaded"
     else:
         return "other"
